35153cc42f8053d787cb22da482b140f52575aba
Ericawanja <wanjaerica@gmail.com>
2023-01-22 15:57:10 +0300

    icons manipulations

--- ecommerce-product-page-main/index.js
+++ ecommerce-product-page-main/index.js
@@ -3,21 +3,50 @@ const minus = document.querySelector(".minus");
 
 const product = document.querySelector(".product");
 
+//display or not display add to cart btn
+
+const manipulationIcons = document.querySelector(".cartPlusMinusIcons");
+const btn = document.querySelector(".addCart");
+const total = document.querySelector(".cartTotal").innerText;
+const displayOrHideCartIcons = (val) => {
+  if (val <= 0) {
+  
+
+    manipulationIcons.style.display = "none";
+    btn.style.display = 'block'
+  } else {
+    manipulationIcons.style.display = "flex";
+    btn.style.display = 'none'
+ 
+  }
+};
+
+//manipulate cart
+
 const addCart = () => {
   const items = document.querySelector(".value").innerText;
+  const cartTotal = document.querySelector(".cartTotal").innerText;
+  const newTotal = (+items + 1).toString();
 
-  document.querySelector(".value").innerText = (+items + 1).toString();
+  document.querySelector(".value").innerText = newTotal;
+  document.querySelector(".cartTotal").innerText = newTotal;
+
+displayOrHideCartIcons(+newTotal)
 };
 
 plus.addEventListener("click", addCart);
 minus.addEventListener("click", () => {
   const items = document.querySelector(".value").innerText;
-  let value = document.querySelector(".value").innerHTML;
-  if (+value <= 0) {
-    return alert("There are no items in the cart");
-  }
-  document.querySelector(".value").innerHTML = (+items - 1).toString();
+  const cartTotal = document.querySelector(".cartTotal").innerText;
+
+  const newTotal = (+items - 1).toString();
+  document.querySelector(".value").innerHTML = newTotal;
+  document.querySelector(".cartTotal").innerText = newTotal;
+
+  displayOrHideCartIcons(+newTotal);
 });
+
+//Change product image
 
 const changeImage = (classes, element, allElements) => {
   for (let i = 0; i < allElements.length; i++) {
@@ -53,3 +82,6 @@ for (let i = 0; i < thumbnails.length; i++) {
     changeImage(thumbnails[i].className, thumbnails[i], thumbnails)
   );
 }
+
+btn.addEventListener("click", addCart);
+displayOrHideCartIcons(0)
