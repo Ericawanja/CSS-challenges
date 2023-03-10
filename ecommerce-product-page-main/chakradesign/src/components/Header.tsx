c110b5a1b152cd8aee9127a4c76b1009ef514da2
Ericawanja <wanjaerica@gmail.com>
2023-03-10 23:48:30 +0300

    header chakra section

---
+++ ecommerce-product-page-main/chakradesign/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+function Header() {
+  return (
+    <Box>
+      <Heading fontWeight='800' fontSize='48px' color='#F&FAFC'>Simple pricing for your business</Heading>
+      <Text>Plans that are carefully crafted to suit your business</Text>
+    </Box>
+  );
+}
+
+export default Header;
